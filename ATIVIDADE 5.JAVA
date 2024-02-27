e73e0cf5c22bc2396a0aee151f0e577725889c1b
yasmimlb <161394259+yasmimlb@users.noreply.github.com>
2024-02-26 21:12:51 -0300

    Create ATIVIDADE 5.JAVA

---
+++ ATIVIDADE 5.JAVA
@@ -0,0 +1,46 @@
+public class ControleProdutos {
+    public static void main(String[] args) {
+    
+    //-------Smartphone---------------
+    String nomeDoSmartphone = "Xiaomi10";
+    String marcaDoSmartphone = "Xiaomi";
+    double precoDoSmartphone = 1500.00;
+    Integer quantidadeEstoqueSmartphone = 5;
+    
+    //---------Laptop------------------
+    String nomeDoLaptop= "Lenovo15LS";
+    String marcaDoLaptop = "Lenovo";
+    double precoDoLaptop = 5500.00;
+    Integer quantidadeEstoqueLaptop = 6;
+    
+    //---------Fone de Ouvido Bluetooth---------------
+    String nomeDoFoneDeOuvidoBluetooth= "XiaomiT2";
+    String marcaDoFoneDeOuvidoBluetooth = "Xiaomi";
+    double precoDoFoneDeOuvidoBluetooth = 500.00;
+    Integer quantidadeFoneDeOuvidoBluetooth = 15;
+    
+    System.out.println("-Smartphone-------------------------------------");
+    System.out.println("Dados do Smartphone:");
+    System.out.println("Nome: " + nomeDoSmartphone);
+    System.out.println("Marca: " + marcaDoSmartphone);
+    System.out.println("Preco: " + precoDoSmartphone);
+    System.out.println("Quantidade: " + quantidadeEstoqueSmartphone);
+    System.out.println("------------------------------------------------");
+    
+    System.out.println("--Laptop----------------------------------------");
+    System.out.println("Dados do Laptop:");
+    System.out.println("Nome: " + nomeDoLaptop);
+    System.out.println("Marca: " + marcaDoLaptop);
+    System.out.println("Preco: " + precoDoLaptop);
+    System.out.println("Quantidade: " + quantidadeEstoqueLaptop);
+    System.out.println("------------------------------------------------");
+    
+    System.out.println("-Laptop---Fone de Ouvido Bluetooth--------------");
+    System.out.println("Dados do Fone de Ouvido Bluetooth:");
+    System.out.println("Nome: " + nomeDoFoneDeOuvidoBluetooth);
+    System.out.println("Marca: " + marcaDoFoneDeOuvidoBluetooth);
+    System.out.println("Preco: " + precoDoFoneDeOuvidoBluetooth);
+    System.out.println("Quantidade: " + quantidadeFoneDeOuvidoBluetooth);
+    System.out.println("------------------------------------------------");
+    }
+}
