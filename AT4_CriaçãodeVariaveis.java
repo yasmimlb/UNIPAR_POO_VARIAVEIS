aa59fce80069034c62c92ff698ca3dd4d0c1ca0c
yasmimlb <161394259+yasmimlb@users.noreply.github.com>
2024-04-17 15:20:21 -0300

    Update and rename ATIVIDADE 4.JAVA to AT4_CriaçãodeVariaveis.java
    1. **Você foi contratado para desenvolver um programa simples em Java para gerenciar o estoque de uma loja de conveniência. A loja vende uma variedade de produtos, incluindo alimentos, bebidas e produtos de higiene. Seu objetivo é criar um sistema para acompanhar a quantidade de cada produto em estoque.**
        
        **Tarefas:**
        
        1. Identifique os diferentes tipos de informações que precisamos armazenar para cada produto.
        2. Declare as variáveis necessárias para representar essas informações.
        3. Considere os tipos de dados apropriados para cada variável.
        
        **Informações a serem armazenadas para cada produto:**
        
        - Nome do produto
        - Categoria do produto (ex: alimentos, bebidas, higiene)
        - Preço unitário do produto
        - Quantidade em estoque do produto
        
        Sua tarefa é identificar e declarar as variáveis necessárias para representar essas informações. Escreva as declarações de variáveis dentro do escopo a seguir:
        
        ```java
        
        public class ControleEstoque {
            public static void main(String[] args) {
                // Declaração das variáveis aqui
        
            }
        }
        
        ```
        
        Lembre-se de escolher nomes significativos para suas variáveis e utilizar os tipos de dados apropriados para cada informação.
        
        Após declarar as variáveis, você pode pensar em como utilizar essas variáveis em operações futuras, como adicionar ou remover itens do estoque.

---
+++ AT4_CriaçãodeVariaveis.java
@@ -0,0 +1,10 @@
+public class ControleEstoque {
+    public static void main(String[] args) {
+        // Declaração das variáveis aqui
+
+        String nomeProduto;
+        String categoriaProduto;
+        double precoUnitario; 
+        int quantidadeEstoque;
+    }
+}
