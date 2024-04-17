d37be798872d1f5c9fbd57e11735075858f11d99
yasmimlb <161394259+yasmimlb@users.noreply.github.com>
2024-04-17 16:21:41 -0300

    Create AT3_EntradaProcesamentoSaida.java
    Faça um sistema que peça 2 notas para o usuario some as duas e divida por 2;

---
+++ AT3_EntradaProcesamentoSaida.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+class SomaDivisaoInteiro {
+    public static void main(String[] args) {
+//Caso seja numeros inteiros, professor nao diz
+     
+      int prNo = 0;
+      int seNo = 0;
+     
+     Scanner scanner = new Scanner(System.in);
+     
+     System.out.println("Informe a primeira nota:");
+     prVa = scanner.nextInt();
+     
+    System.out.println("Informe a segunda nota:");
+    seVa = scanner.nextInt();
+
+    int total = (prNo + seNo) / 2 ;
+    
+    System.out.println("A nota da média é :" + total);
+    }
+}
+
+
+import java.util.Scanner;
+
+public class SomaDivisaoDecimal {
+    public static void main(String[] args) {
+//Caso seja numeros decimais, professor nao diz
+
+        double prNo = 0.0;
+        double seNo = 0.0;
+     
+        Scanner scanner = new Scanner(System.in);
+     
+        System.out.println("Informe a primeira nota:");
+        primeiraNota = scanner.nextDouble();
+     
+        System.out.println("Informe a segunda nota:");
+        segundaNota = scanner.nextDouble();
+
+        double media = (prNo+ seNo) / 2 ;
+    
+        System.out.println("A nota da média é :" + total);
+    }
+}
